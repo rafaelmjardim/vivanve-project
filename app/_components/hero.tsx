4401f34ce629ec92557a4e1fa5eadffb5ff53be6
rafaelmjardim <rafaelmjardim@hotmail.com>
2026-01-21 07:56:15 -0300

    refactor: cria compponente de heroActions e concentra use client apenas nele

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -1,12 +1,8 @@
-"use client";
-
 import Image from "next/image";
 import { Header } from "./ui/header";
 import heroBackground from "@/public/hero-background.jpg";
-import { Button } from "./ui/button";
-import { HiArrowSmallRight } from "react-icons/hi2";
 import { MouseAnimate } from "./ui/mouseAnimate";
-import { linkWhatsApp, scrollSection } from "../_utils/utils";
+import { HeroActions } from "./heroActions";
 
 export function Hero() {
   return (
@@ -39,19 +35,7 @@ export function Hero() {
             civil.
           </p>
 
-          <div className="flex items-center gap-4 flex-col sm:flex-row">
-            <Button className="w-full sm:w-fit" onClick={() => linkWhatsApp()}>
-              Solicitar orçamento
-              <HiArrowSmallRight size={20} />
-            </Button>
-            <Button
-              className="w-full sm:w-fit"
-              isTransparent={true}
-              onClick={() => scrollSection("projectCatalog")}
-            >
-              Ver catálogo
-            </Button>
-          </div>
+          <HeroActions />
         </div>
       </article>
 
